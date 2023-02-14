a7d9fe7eb8c159769a151c3015734c84e2256df3
Melroy van den Berg <melroy@melroy.org>
2023-02-14 23:13:53 +0100

    More generic codespell on md files

--- spell-check.sh
+++ spell-check.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
-codespell ./content/about.md &&
-  codespell ./content/_index.md &&
+codespell ./content/*.md &&
   codespell ./content/posts/{2018,2019,202*,203*,204*,205*,206*,207*,208*}-*
